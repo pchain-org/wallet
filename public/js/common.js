824996845df23d480f4204a680ee48bff95c6639
skykingit <skykingit@aliyun.com>
2019-01-04 16:56:22 +0800

    delete useless functions

--- public/js/common.js
+++ public/js/common.js
@@ -1,52 +1,52 @@
 /**
- * Created by skykingit on 2017/4/19.
+ * Created by skykingit on 2018/12/30.
  */
 const Web3 = require("pweb3");
 const EthTx = require("pchainjs-tx");
 const TxData = require("txdata");
 const CryptoJS = require("crypto-js");
 const BigNumber = require('bignumber.js');
-window.angularApp = angular.module('myApp',[]);
+window.angularApp = angular.module('myApp', []);
 
-function AESEncrypt(msg,password){
-    return CryptoJS.AES.encrypt(msg,password).toString();
+function AESEncrypt(msg, password) {
+    return CryptoJS.AES.encrypt(msg, password).toString();
 }
 
-function AESDecrypt(enMsg,password){
-    return CryptoJS.AES.decrypt(enMsg,password).toString(CryptoJS.enc.Utf8);
+function AESDecrypt(enMsg, password) {
+    return CryptoJS.AES.decrypt(enMsg, password).toString(CryptoJS.enc.Utf8);
 }
 
-function loading(){
+function loading() {
     var html = '<div class="loading"><div class="pic"><div class="myloader"></div></div></div>';
     $('body').append(html);
 }
 
-function removeLoading(){
+function removeLoading() {
     $('body').find('.loading').remove();
 }
 
-function removePageLoader(){
+function removePageLoader() {
     $(".page-loader-wrapper").hide();
 }
 
-setTimeout(removePageLoader,3000);
+setTimeout(removePageLoader, 1500);
 
-function showPopup(str,time,callback){
-    if(!time) time = 3000;
-    var html = '<div class="popup"><div class="pContent"><p>'+str+'</p></div></div>';
+function showPopup(str, time, callback) {
+    if (!time) time = 3000;
+    var html = '<div class="popup"><div class="pContent"><p>' + str + '</p></div></div>';
     $('body').append(html);
-    setTimeout(function () {
+    setTimeout(function() {
         removePopup();
-        if(callback)
+        if (callback)
             callback();
-    },time)
+    }, time)
 }
 
-function removePopup(){
+function removePopup() {
     $('body').find('.popup').remove();
 }
 
-function showNotification(colorName, text, placementFrom, placementAlign, animateEnter, animateExit,time) {
+function showNotification(colorName, text, placementFrom, placementAlign, animateEnter, animateExit, time) {
     if (colorName === null || colorName === '') { colorName = 'bg-black'; }
     if (text === null || text === '') { text = 'Turning standard Bootstrap alerts'; }
     if (animateEnter === null || animateEnter === '') { animateEnter = 'animated fadeInDown'; }
@@ -57,21 +57,20 @@ function showNotification(colorName, text, placementFrom, placementAlign, animat
 
     $.notify({
         message: text
-    },
-        {
-            type: colorName,
-            allow_dismiss: allowDismiss,
-            newest_on_top: true,
-            timer: time,
-            placement: {
-                from: placementFrom,
-                align: placementAlign
-            },
-            animate: {
-                enter: animateEnter,
-                exit: animateExit
-            },
-            template: '<div data-notify="container" class="bootstrap-notify-container myNotify alert alert-dismissible {0} ' + (allowDismiss ? "p-r-35" : "") + '" role="alert">' +
+    }, {
+        type: colorName,
+        allow_dismiss: allowDismiss,
+        newest_on_top: true,
+        timer: time,
+        placement: {
+            from: placementFrom,
+            align: placementAlign
+        },
+        animate: {
+            enter: animateEnter,
+            exit: animateExit
+        },
+        template: '<div data-notify="container" class="bootstrap-notify-container myNotify alert alert-dismissible {0} ' + (allowDismiss ? "p-r-35" : "") + '" role="alert">' +
             '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
             '<span data-notify="icon"></span> ' +
             '<span data-notify="title">{1}</span> ' +
@@ -81,24 +80,24 @@ function showNotification(colorName, text, placementFrom, placementAlign, animat
             '</div>' +
             '<a href="{3}" target="{4}" data-notify="url"></a>' +
             '</div>'
-        });
+    });
 }
 
-function successNotify(text){
-    showNotification("alert-success",text,"bottom","center",400000);
+function successNotify(text) {
+    showNotification("alert-success", text, "bottom", "center", 400000);
 }
 const Clipboard = require("clipboard");
 var clipboard = new Clipboard('.copyBtn');
 
 clipboard.on('success', function(e) {
-    showNotification("alert-success","Copy successfully","bottom","center",1000);
+    showNotification("alert-success", "Copy successfully", "bottom", "center", 1000);
 });
 
-function menuActive(index){
-    var menuEle =  $("html #leftsidebar .menu li");
-    if(menuEle.length < 1){
-        setTimeout(function(){  menuActive(index); },2000);
-    }else{
+function menuActive(index) {
+    var menuEle = $("html #leftsidebar .menu li");
+    if (menuEle.length < 1) {
+        setTimeout(function() { menuActive(index); }, 2000);
+    } else {
         menuEle.removeClass("active");
         menuEle.removeClass("open");
         menuEle.eq(index).addClass("active");
@@ -107,20 +106,21 @@ function menuActive(index){
 }
 
 function GetQueryString(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)","i");
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
     var r = window.location.search.substr(1).match(reg);
-    if (r!=null) return (decodeURI(r[2])); return null;
+    if (r != null) return (decodeURI(r[2]));
+    return null;
 }
 
-function priToAddress(pri){
-    if(pri.length == 64) {
+function priToAddress(pri) {
+    if (pri.length == 64) {
         pri = "0x" + pri;
     };
-    var addr = "0x"+ethUtil.privateToAddress(pri).toString('hex');
+    var addr = "0x" + ethUtil.privateToAddress(pri).toString('hex');
     return addr;
 }
 
-function signTx(pri,txObj){
+function signTx(pri, txObj) {
 
     //console.log(arguments);
     // var Tx = EthTx;
@@ -134,15 +134,15 @@ function signTx(pri,txObj){
 
     var serializedTx = tx.serialize();
 
-    var txData = "0x" +serializedTx.toString('hex');
+    var txData = "0x" + serializedTx.toString('hex');
 
     return txData;
 }
 
 
-function convert(num){
+function convert(num) {
     var x = new BigNumber(num);
-    var s = "0x"+x.toString(16);
+    var s = "0x" + x.toString(16);
     // console.log(num,s);
     return s;
 }
@@ -159,267 +159,64 @@ function sanitizeHex(hex) {
 };
 
 
- 
+
 function decimalToHex(dec) {
     return new BigNumber(dec).toString(16);
 };
-function getHexValue(val){
-    var s =  sanitizeHex(decimalToHex(val));
+
+function getHexValue(val) {
+    var s = sanitizeHex(decimalToHex(val));
 
     // console.log(s);
 }
 
-
-
-function initSignRawDeposite(address,amount,hash,nonce,gasPrice,gasLimit,chainId,type) {
-    //console.log(address,amount,hash,nonce,gasPrice,gasLimit,chainId,type);
-
-    var funcName = "";
-    var kvArr = new Array();
-    var fromObj = {"Key": "from","Value": address};
-    kvArr.push(fromObj);
-
-    if(type == 0){
-        funcName = "DepositInMainChain";
-        var chainIdObj = {"Key": "chainId","Value": chainId};
-        kvArr.push(chainIdObj);
-
-        var amountObj = {"Key": "amount","Value": convert(amount)};
-        kvArr.push(amountObj);
-    }else if(type == 1){
-        funcName = "DepositInChildChain";
-        var chainIdObj = {"Key": "chainId","Value": chainId};
-        kvArr.push(chainIdObj);
-
-        var hashObj = { "Key": "txHash","Value": hash};
-        kvArr.push(hashObj);
-    }else if(type == 2){
-        funcName = "WithdrawFromChildChain";
-        var amountObj = {"Key": "amount","Value": convert(amount)};
-        kvArr.push(amountObj);
-    }else if(type == 3){
-        funcName = "WithdrawFromMainChain";
-        var chainIdObj = {"Key": "chainId","Value": chainId};
-        kvArr.push(chainIdObj);
-        var hashObj = { "Key": "txHash","Value": hash};
-        kvArr.push(hashObj);
-    }
-
-    var obj = {
-        FuncName: funcName,
-        "Params": {
-            "KVArray": kvArr
-        }
-    };
-
-    //console.log(obj);
-
-    var buffer = require('buffer').Buffer;
-
-    var objStr = JSON.stringify(obj);
-    const extendTxData = buffer.from(objStr );
-
-    const rawTx = {
-        nonce: convert(nonce),
-        gasPrice:convert(gasPrice),
-        gasLimit: convert(gasLimit),
-        extendTxData:extendTxData
-    };
-
-    return rawTx;
-}
-
-function initSignRawDeposite2(address,amount,nonce,gasPrice,gasLimit,chainId) {
-    //console.log(address,amount,nonce,gasPrice,gasLimit);
-    var obj = {
-        FuncName: "DepositInMainChain",
-        "Params": {
-            "KVArray": [{
-                "Key": "from",
-                "Value": address
-            },
-                {
-                    "Key": "chainId",
-                    "Value": chainId
-                },
-                {
-                    "Key": "amount",
-                    "Value": convert(amount)
-                }]
-        }
-    };
-
-    //console.log(obj);
-
-    var buffer = require('buffer').Buffer;
-
-    var objStr = JSON.stringify(obj);
-    const extendTxData = buffer.from(objStr );
-
-    const rawTx = {
-        nonce: convert(nonce),
-        gasPrice:convert(gasPrice),
-        gasLimit: convert(gasLimit),
-        extendTxData:extendTxData
-    };
-
-    return rawTx;
-}
-
-function initSignRawDeposite3(address,hash,nonce,gasPrice,gasLimit,chainId) {
-    //console.log(address,hash,nonce,gasPrice,gasLimit);
-    var obj = {
-        FuncName: "DepositInChildChain",
-        "Params": {
-            "KVArray": [{
-                "Key": "from",
-                "Value": address
-            },
-                {
-                    "Key": "chainId",
-                    "Value": chainId
-                },
-                {
-                    "Key": "txHash",
-                    "Value": hash
-                }]
-        }
-    };
-
-    var buffer = require('buffer').Buffer;
-
-    var objStr = JSON.stringify(obj);
-    const extendTxData = buffer.from(objStr );
-
-    const rawTx = {
-        nonce: convert(nonce),
-        gasPrice:convert(0),
-        gasLimit: convert(0),
-        extendTxData:extendTxData
-    };
-
-    return rawTx;
-}
-
-function getChainIdHash(chainId) {
-    var hash = "";
-    switch(chainId){
-        case 0:
-        hash = "pchain";
-        break;
-        case 1:
-        hash = "child_0";
-        break;
-        case 2:
-        hash = "child_1";
-        break;
-        case 3:
-        hash = "child_2";
-        break;
-        case 4:
-        hash = "child_3";
-        break;
-        default:
-        hash = "pchain";
-    }
-
-    return hash;
-}
-
-
-function initSignRawPAI(toAddress,amount,nonce,gasPrice,gasLimit,chainId) {
+function initSignRawPAI(toAddress, amount, nonce, gasPrice, gasLimit, chainId) {
     //console.log(toAddress,amount,nonce,gasPrice,gasLimit);
-   
+
     const rawTx = {
         nonce: convert(nonce),
-        gasPrice:convert(gasPrice),
+        gasPrice: convert(gasPrice),
         gasLimit: convert(gasLimit),
-        to:toAddress,
-        value:convert(amount),
-        chainId:chainId
+        to: toAddress,
+        value: convert(amount),
+        chainId: chainId
     };
 
     return rawTx;
 }
 
-function initSignRawCrosschain(data,nonce,gasPrice,gasLimit,chainId) {
+function initSignRawCrosschain(data, nonce, gasPrice, gasLimit, chainId) {
     const rawTx = {
         nonce: convert(nonce),
-        gasPrice:convert(gasPrice),
+        gasPrice: convert(gasPrice),
         gasLimit: convert(gasLimit),
-        data:data,
-        to:"0x0000000000000000000000000000000000000065",
-        chainId:getChainIdHash(chainId)
+        data: data,
+        to: "0x0000000000000000000000000000000000000065",
+        chainId: chainId
     };
     return rawTx;
 }
 
-function initSignRawContract(toAddress,data,nonce,gasPrice,gasLimit,amount,chainId) {
+function initSignRawContract(toAddress, data, nonce, gasPrice, gasLimit, amount, chainId) {
     const rawTx = {
         nonce: convert(nonce),
-        gasPrice:convert(gasPrice),
+        gasPrice: convert(gasPrice),
         gasLimit: convert(gasLimit),
-        to:toAddress,
-        data:data,
-        value:convert(amount),
-        chainId:getChainIdHash(chainId)
-    };
-    return rawTx;
-}
-
-function initSignRawDeployContract(data,nonce,gasPrice,gasLimit,amount,chainId){
-    const rawTx = {
-        nonce: convert(nonce),
-        gasPrice:convert(gasPrice),
-        gasLimit: convert(gasLimit),
-        data:data,
-        value:convert(amount),
-        chainId:getChainIdHash(chainId)
+        to: toAddress,
+        data: data,
+        value: convert(amount),
+        chainId: chainId
     };
     return rawTx;
 }
 
 var APIHost = "https://air.pchain.wang";
 
-// var APIHost = "";
-
-function fliterIPList(list){
-    var listObj = {};
-    listObj.California = new Array();
-    listObj.Oregon = new Array();
-    listObj.Virginia = new Array();
-    listObj.London = new Array();
-    listObj.Singapore = new Array();
-    listObj.Tokyo = new Array();
-    listObj.Frankfurt = new Array();
-    var nodeCounts = {
-        "California":0,
-        "Oregon": 0,
-        "Virginia": 0,
-        "London": 0,
-        "Singapore": 0,
-        "Tokyo": 0,
-        "Frankfurt":0
-    }
-    for(var i=0;i<list.length;i++){
-        var current = list[i];
-        if(listObj[current.areaname].length < 100)
-        listObj[current.areaname].push(current.ip);
-        nodeCounts[current.areaname]++;
-    }
-    var result = {};
-    result.ipList = listObj;
-    result.nodeList = nodeCounts;
-    return result;
-}
-
-var crossChainABI = [
-    {
+var crossChainABI = [{
         "type": "function",
         "name": "DepositInMainChain",
         "constant": false,
-        "inputs": [
-            {
+        "inputs": [{
                 "name": "chainId",
                 "type": "string"
             },
@@ -434,8 +231,7 @@ var crossChainABI = [
         "type": "function",
         "name": "DepositInChildChain",
         "constant": false,
-        "inputs": [
-            {
+        "inputs": [{
                 "name": "chainId",
                 "type": "string"
             },
@@ -450,8 +246,7 @@ var crossChainABI = [
         "type": "function",
         "name": "WithdrawFromChildChain",
         "constant": false,
-        "inputs": [
-            {
+        "inputs": [{
                 "name": "chainId",
                 "type": "string"
             },
@@ -466,8 +261,7 @@ var crossChainABI = [
         "type": "function",
         "name": "WithdrawFromMainChain",
         "constant": false,
-        "inputs": [
-            {
+        "inputs": [{
                 "name": "chainId",
                 "type": "string"
             },
@@ -483,14 +277,3 @@ var crossChainABI = [
         "outputs": []
     }
 ]
-
-var childChainAmount = 2;
-
-
-
-
-
-
-
-
-
